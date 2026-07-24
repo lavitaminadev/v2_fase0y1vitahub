35f897a86ca74a66a8eb8fafb9840673839e56e1
La Vitamina Dev <dev@lavitamina.com>
2026-07-24 11:41:52 -0400

    fix: ClientOverviewService faltante en providers del modulo clients

--- apps/api/src/modules/clients/clients.module.ts
+++ apps/api/src/modules/clients/clients.module.ts
@@ -5,12 +5,13 @@ import { ClientsController } from './clients.controller';
 import { CreateClientUseCase } from './create-client.use-case';
 import { ListClientsUseCase } from './list-clients.use-case';
 import { GetClientUseCase } from './get-client.use-case';
+import { ClientOverviewService } from './client-overview.service';
 import { User } from '../users/user.entity';
 import { Lead } from '../crm/leads/lead.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Client, User, Lead])],
   controllers: [ClientsController],
-  providers: [CreateClientUseCase, ListClientsUseCase, GetClientUseCase],
+  providers: [CreateClientUseCase, ListClientsUseCase, GetClientUseCase, ClientOverviewService],
 })
 export class ClientsModule {}
